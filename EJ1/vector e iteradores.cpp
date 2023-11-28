c2f59cfdff3a38323911bb29d88bbe050d13a9ee
ZoeAcero <zar.amazona@gmail.com>
2023-11-28 10:40:08 +0100

    2

--- EJ1/vector e iteradores.cpp
+++ EJ1/vector e iteradores.cpp
@@ -2,8 +2,21 @@
 #include <vector>
 
 int main() {
-    
+
     std::vector<int> numeros;
     for (int i = 1; i <= 5; ++i) {
         numeros.push_back(i * 10);
     }
+
+    for (const auto &numero : numeros) {
+       
+        auto imprimirDoble = [](int n) {
+            std::cout << "El doble de " << n << " es " << n * 2 << std::endl;
+        };
+
+        imprimirDoble(numero);
+    }
+
+    return 0;
+}
+
