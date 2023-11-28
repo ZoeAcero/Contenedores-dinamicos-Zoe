01f865b279434d9aa47b47af34ef406034d485bd
ZoeAcero <zar.amazona@gmail.com>
2023-11-28 11:14:49 +0100

    2

--- EJ5/C++ moderno.cpp
+++ EJ5/C++ moderno.cpp
@@ -7,3 +7,10 @@ class Transformacion {
 public:
     virtual int aplicar(int num) const = 0;
 };
+
+class Cuadrado : public Transformacion {
+public:
+    int aplicar(int num) const override {
+        return num * num;
+    }
+};
