9c177dd82d7d971f97ccd6a5e3ec30837e49584a
ZoeAcero <zar.amazona@gmail.com>
2023-11-28 11:15:46 +0100

    3

--- EJ5/C++ moderno.cpp
+++ EJ5/C++ moderno.cpp
@@ -14,3 +14,20 @@ public:
         return num * num;
     }
 };
+
+template <typename T>
+void imprimirElemento(const T& elemento, const std::string& mensaje = "") {
+    std::cout << mensaje << elemento << std::endl;
+}
+
+int main() {
+
+    Cuadrado cuadradoTransformacion;
+    std::cout << "Cuadrado de 5: " << cuadradoTransformacion.aplicar(5) << std::endl;
+
+    std::vector<int> numeros = {1, 2, 3, 4, 5};
+
+
+    std::for_each(numeros.begin(), numeros.end(), [](int num) {
+        imprimirElemento(num, "Elemento: ");
+    });
