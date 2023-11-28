c72ab5ba2ddcd913792c96bc5ada60e61e7d1297
ZoeAcero <zar.amazona@gmail.com>
2023-11-28 10:43:39 +0100

    2

--- EJ2/contenedores estandar y secuencias.cpp
+++ EJ2/contenedores estandar y secuencias.cpp
@@ -8,3 +8,17 @@ template<typename T>
 void imprimirElemento(const T& elemento) {
     std::cout << "Elemento: " << elemento << std::endl;
 }
+
+template<typename T>
+void manipularElemento(T& elemento) {
+    elemento += 1;
+}
+
+int main() {
+    std::list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
+    std::stack<float, std::list<float>> pila(lista);
+
+    
+    auto imprimirLambda = [](const float& elemento) {
+        std::cout << "Elemento desde lambda: " << elemento << std::endl;
+    };
