b2df640267b2746eea973cb21b33b3b30d6f874e
ZoeAcero <zar.amazona@gmail.com>
2023-11-28 10:44:56 +0100

    3

--- EJ2/contenedores estandar y secuencias.cpp
+++ EJ2/contenedores estandar y secuencias.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <list>
 #include <stack>
-#include <algorithm>
-#include <functional>
 
 template<typename T>
 void imprimirElemento(const T& elemento) {
@@ -18,7 +16,30 @@ int main() {
     std::list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
     std::stack<float, std::list<float>> pila(lista);
 
-    
+
     auto imprimirLambda = [](const float& elemento) {
         std::cout << "Elemento desde lambda: " << elemento << std::endl;
     };
+
+    while (!pila.empty()) {
+
+        imprimirElemento(pila.top());
+
+        imprimirLambda(pila.top());
+
+        auto manipularLambda = [](float& elemento) {
+            elemento *= 2; // Duplicar cada elemento en la pila
+        };
+        manipularLambda(pila.top());
+
+        imprimirElemento(pila.top());
+
+        manipularElemento(pila.top());
+
+        imprimirElemento(pila.top());
+
+        pila.pop();
+    }
+
+    return 0;
+}
