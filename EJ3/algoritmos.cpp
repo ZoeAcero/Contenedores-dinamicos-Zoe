9ef6050ceaf9c9ca78541e18cf123dad92f6a35d
ZoeAcero <zar.amazona@gmail.com>
2023-11-28 10:48:39 +0100

    2

--- EJ3/algoritmos.cpp
+++ EJ3/algoritmos.cpp
@@ -10,7 +10,27 @@ void imprimirElemento(const T& elemento, const std::string& mensaje = "") {
 
 int main() {
     std::vector<int> numeros = {50, 20, 60, 10, 30, 40};
-    
+
     std::for_each(numeros.begin(), numeros.end(), [](int num) {
         imprimirElemento(num, "Elemento ordenado: ");
     });
+
+    std::cout << "Elementos en orden inverso:" << std::endl;
+    std::for_each(numeros.rbegin(), numeros.rend(), [](int num) {
+        imprimirElemento(num);
+    });
+
+    auto minElement = std::min_element(numeros.begin(), numeros.end());
+    auto maxElement = std::max_element(numeros.begin(), numeros.end());
+
+    imprimirElemento(*minElement, "Número mínimo: ");
+    imprimirElemento(*maxElement, "Número máximo: ");
+
+   
+    std::cout << "Elementos en orden inverso con iteradores inversos:" << std::endl;
+    for (auto it = std::prev(numeros.end()); it != std::prev(numeros.begin()); --it) {
+        imprimirElemento(*it);
+    }
+
+    return 0;
+}
